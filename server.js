9c00caf3d5d27acf68833b263b68948ead3a5937
SakshiSingh-12 <139615391+SakshiSingh-12@users.noreply.github.com>
2024-09-28 11:29:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -125,3 +125,14 @@ app.get('/api/user_image/:id', (req, res) => {
         }
     });
 });
+
+
+// Delete user route
+app.delete('/api/delete_user/:id', (req, res) => {
+    const userId = req.params.id;
+    let sql = 'DELETE FROM users WHERE id = ?';
+    db.query(sql, [userId], (err, result) => {
+        if (err) throw err;
+        res.sendStatus(200);
+    });
+});
