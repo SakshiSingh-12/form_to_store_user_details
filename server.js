ddeb1f085a3845613e909c45565862080bad6a72
SakshiSingh-12 <139615391+SakshiSingh-12@users.noreply.github.com>
2024-10-05 11:16:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const multer = require('multer');
-
+const path = require('path');
 
 
 const app = express();
